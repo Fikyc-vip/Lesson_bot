198c45ae0695f5a96f409414be1ba4f2adbda4a8
Fikyc-vip <Fikyc@mail.ru>
2025-07-23 21:57:20 +0300

    KS 1-4 Решение задачи 4. Создание переменных по типу и длине

---
+++ app/src/main/kotlin/Lesson1/KS-1-4.kt
@@ -0,0 +1,24 @@
+package Lesson1
+
+fun main() {
+
+    val length: Long = 40_868_600_000L
+
+    val age: Byte = 27
+
+    val partOfDay: Float = 0.075f
+
+    val seconds: Short = 6480
+
+    val partOfYear: Double = 2.0547945205479453E-4
+
+    val apogee: Int = 327000
+
+    println("length: $length mm")
+    println("age: $age years")
+    println("partOfDay: $partOfDay")
+    println("seconds: $seconds s")
+    println("partOfYear: $partOfYear")
+    println("apogee: $apogee m")
+
+}
