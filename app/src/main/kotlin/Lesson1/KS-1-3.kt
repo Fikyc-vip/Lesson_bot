d69f9ba31f89a79ced49be3aa6638a5bda96e526
Fikyc-vip <Fikyc@mail.ru>
2025-07-23 21:48:36 +0300

    KS 1-3 Решение задачи 3. Создание переменных var

---
+++ app/src/main/kotlin/Lesson1/KS-1-3.kt
@@ -0,0 +1,14 @@
+package Lesson1
+
+fun main() {
+    val year = 1961
+    var hour = 9
+    var minute = 7
+    println("$year")
+    println("$hour")
+    print("$minute")
+    hour = 10
+    minute = 55
+    println("")
+    println("$hour:$minute")
+}
