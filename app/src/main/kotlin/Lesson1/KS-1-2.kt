2d271671a3d138b73afe977c59100290cb48e02b
Fikyc-vip <Fikyc@mail.ru>
2025-07-23 21:35:09 +0300

    KS 1-2 Решение задачи 2. Создал переменную "число рабочих", вычел 1, распечатал новое значение

---
+++ app/src/main/kotlin/Lesson1/KS-1-2.kt
@@ -0,0 +1,17 @@
+package Lesson1
+
+import android.util.Printer
+import kotlin.math.truncate
+
+fun main() {
+    val numberOfOrders: Int = 75
+    val text: String = "Спасибо за заказ"
+    println("$numberOfOrders")
+    println("$text")
+    var numberOfEmployees : Int = 2000
+    println("Число работников $numberOfEmployees")
+    numberOfEmployees --
+    println("Новое число работников $numberOfEmployees")
+
+
+}
