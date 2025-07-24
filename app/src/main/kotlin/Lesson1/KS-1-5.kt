6b0505b90543b5e4f2dcfb900537b6d4d7f00f9d
Fikyc-vip <Fikyc@mail.ru>
2025-07-24 22:53:56 +0300

    KS 1-5 Внёс исправления, удалил лишний файл

--- app/src/main/kotlin/Lesson1/KS-1-5.kt
+++ app/src/main/kotlin/Lesson1/KS-1-5.kt
@@ -3,16 +3,19 @@ package Lesson1
 import android.util.Log
 
 fun main() {
+
+    val oneHour = 60
     val totalSeconds = 6480
 
-    val hours = totalSeconds / 3600
-    val remainingAfterHours = totalSeconds % 3600
+    val hours = totalSeconds / (oneHour*oneHour)
+    val remainingAfterHours = totalSeconds % (oneHour*oneHour)
 
-    val minutes = remainingAfterHours / 60
-    val seconds = remainingAfterHours % 60
+    val minutes = remainingAfterHours / oneHour
+    val seconds = remainingAfterHours % oneHour
 
-    val formattedTime = "%02d:%02d:%02d".format(hours, minutes, seconds)
-    println("Время полёта: $formattedTime")
+//    val formattedTime = "%02d:%02d:%02d".format(hours, minutes, seconds)
+//    println("Время полёта: $formattedTime")
+    println("%02d:%02d:%02d".format(hours, minutes, seconds))
     
 }
 
