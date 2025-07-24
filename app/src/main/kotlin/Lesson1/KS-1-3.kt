29cc061e06e002a8906b810f70e10cceb66dca37
Fikyc-vip <Fikyc@mail.ru>
2025-07-24 22:28:48 +0300

    KS 1-3 Внёс исправления

--- app/src/main/kotlin/Lesson1/KS-1-3.kt
+++ app/src/main/kotlin/Lesson1/KS-1-3.kt
@@ -2,13 +2,13 @@ package Lesson1
 
 fun main() {
     val year = 1961
-    var hour = 9
-    var minute = 7
+    var hour: String = "09"
+    var minute: String = "07"
     println("$year")
     println("$hour")
     print("$minute")
-    hour = 10
-    minute = 55
+    hour = "10"
+    minute = "55"
     println("")
-    println("$hour:$minute")
+    println("[$hour:$minute]")
 }
